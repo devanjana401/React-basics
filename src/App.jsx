65b97fa494770cc9e85f791a3d736018994e5398
devanjana401 <anjanasasiaduvad@gmail.com>
2025-11-05 16:04:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Basic from './embedding-expression/basic'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* -----embedding expresiion----- */}
+      <Basic/>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/embedding-expression/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = () => {
+    const name = 'Anjana';
+  return (
+    <div>
+      <h2>Hi {name}</h2>
+    </div>
+  )
+}
+
+export default Basic
